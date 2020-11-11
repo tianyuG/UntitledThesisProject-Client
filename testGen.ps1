5c03f73e9de08f3087a85e0d851b9d155303f906
Tianyu Ge <tianyu@vt.edu>
2020-11-11 15:48:56 -0500

    Update UI for critique

---
+++ testGen.ps1
@@ -0,0 +1,9 @@
+﻿## Find all BackupState.txt files in C:\ARCHIVE_VOLUMES
+$files = Get-ChildItem C:\Users\Tianyu\Test\1\*.jpg
+
+Set-Location -Path C:\Users\Tianyu\Test\2
+
+## Read the contents of each file
+foreach ($file in $files) {
+    magick.exe $($file.FullName) -dither FloydSteinberg -colors 6 .\$($file.Name)
+}

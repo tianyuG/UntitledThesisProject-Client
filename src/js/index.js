4efbee58bd48e5313c526eb3f22ec0a1590f4fd7
Tianyu Ge <tianyu@vt.edu>
2021-03-10 17:05:26 -0500

    Increased timeout

--- src/js/index.js
+++ src/js/index.js
@@ -88,7 +88,7 @@ commitSearch.addEventListener("click", () => {
     // mainContent.openDevTools();
 
     // NEEDSWORK: If dom-ready event is observed, clicking the button will fire off getSearchResults with all previous search terms, and the result will be unpredictable. If dom-ready is not observed, sometimes clicking on the button will result in a blank page as the page was not fully loaded. Setting 500ms delay is a temp fix.
-    setTimeout(getSearchResults(searchTerm), 500);
+    setTimeout(getSearchResults(searchTerm), 1250);
 
     // wiki()
     //   .page(searchTerm)

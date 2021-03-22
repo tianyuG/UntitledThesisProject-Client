7ffad97de1a3cecf864ce7e4cba389beb269c8f3
Tianyu Ge <tianyu@vt.edu>
2021-03-22 09:41:20 -0400

    Updated license page

---
+++ src/js/licensesplash.js
@@ -0,0 +1,34 @@
+const { ipcRenderer, remote } = require("electron");
+
+document.onkeydown = function (e) {
+  e = e || window.event;
+  if (e.key == "Escape" || e.key == "q") {
+    closeSelf();
+  }
+};
+
+document.body.addEventListener("click", closeSelf, true);
+
+document.body.addEventListener(
+  "mousemove",
+  () => {
+    console.log("playaudio");
+    const audioContext = new AudioContext();
+    var la0 = document.getElementById("la-preloop");
+    var la1 = document.getElementById("la-loop-noremainder");
+    var la2 = document.getElementById("la-loop");
+    const ael1 = document.getElementById("la-loop-noremainder");
+    const tr1 = audioContext.createMediaElementSource(ael1);
+    tr1.connect(audioContext.destination);
+  },
+  { once: true }
+);
+
+function closeSelf() {
+  ipcRenderer.send("doActivateMainTitleBar");
+  var currWin = remote.getCurrentWindow();
+  currWin.close();
+}
+
+// TODO: Do this in aboutModal!
+ipcRenderer.on("play-audio", () => {});
